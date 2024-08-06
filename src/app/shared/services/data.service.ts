1170f1872874cb4251f8b77906caa69615648a47
Jonathan Daniel <47608996+JDev-il@users.noreply.github.com>
2024-08-06 10:20:20 +0300

    Update data.service.ts

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../../core/services/api.service';
-import { RxService } from './rx.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  constructor(private rxService: RxService, private apiService: ApiService) {
+  constructor(private apiService: ApiService) {
   }
 }
